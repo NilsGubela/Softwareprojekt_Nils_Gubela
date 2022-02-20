772038ae06dc1aa7ada80c22860279fe7b75b72b
NilsGubela <nilsgubela94@gmail.com>
2022-02-20 21:38:01 +0100

    massive clean up + merging of code
    All files but code for pausing site fining algo were removed. One script for adaptive walk and exhaustive walk. Flags for DrTransformer added.

--- code/pause_finding.py
+++ code/pause_finding.py
@@ -4,8 +4,25 @@ import subprocess
 import math
 import numpy as np
 import time
+import argparse
 start_time = time.time()
-#import matplotlib.pyplot as plt
+parser = argparse.ArgumentParser()
+
+# obtain values of command line flags
+parser.add_argument("-m", "--mode", dest = "mode" ,help="Either adaptive walk or exhaustive search", default = "adaptive", type = str)
+parser.add_argument("-i", "--input", dest ="file", help="Name of file that contains sequence structure pairs", type = str)
+#additional arguments of DrTransformer
+parser.add_argument("--o-prune", dest = "prune", help=" Occupancy threshold to prune structures from the network. The structures with lowest occupancy are removed until at most o-prune occupancy has been removed from the total population. (default: 0.05)", default = 0.05, type = float)
+parser.add_argument("-T", "--temp", dest = "temp", help=" Rescale energy parameters to a temperature of temp C. (default: 37.0)",default = 37.0, type=float)
+
+args = parser.parse_args()
+
+mode = args.mode
+file_name = args.file
+prune = args.prune
+temp = args.temp
+
+
 
 # Simulation parameter
 _RT = 0.61632077549999997
@@ -59,12 +76,10 @@ def dr_list(filename):
 
 
 
-# length of pause is set to 20 seconds
-#pause_len = 100
 
 # obtain sequence and structure from base.txt
 # read in base file
-f = open("base.txt", "r")
+f = open(file_name, "r")
 meta = []
 for x in f:
 	if x.find('\n') != -1:
@@ -72,28 +87,38 @@ for x in f:
 	else:
 		meta.append(x.split(" "))
 
-# perform exhaustive search for all sequence structure pairs in base.txt
+
 for line in meta:
+
+	# if all seq-struc pairs have been processed, end program
 	if line == " ":
 		break
+
+	# time is monitored and later printed to the command line
 	start_time = time.time()
+
+	# initialize variables
 	seq = line[0]
 	struc = line[1]
-	print("Structure "+struc+ " processing now")
 	n = len(seq)
-	# call DrTransformer without pausing site, controll
-	subprocess.call(["echo "+seq+" | DrTransformer --logfile"], shell=True)
+
+	# let the user know which seq-struc pair is currently processed
+	print("Sequence " + seq + " and structure "+ struc + " processing now")
+
+	# call DrTransformer without pausing site
+	subprocess.call(["echo "+seq+" | DrTransformer --logfile"  + " --o-prune " + str(prune) + " -T " + str(temp)], shell=True)
 	# read in list from DrTransformer logfile
 	liste, liste_end = dr_list("NoName.log")
-	new_ratio = 0
+	
 	# find structure rate at end of transcritption
+	new_ratio = 0
 	for i in range(0,len(liste_end)):
 		if liste_end[i][2] == struc:
-			new_ratio = float(liste_end[i][6])
+			new_ratio = float(liste_end[i][4])
 	# set highscore for all other calculations
 	highscore = new_ratio
 	old_highscore = highscore
-	# search for all steps with no structure alternative in the beginning
+	# search for all steps with no structure alternative in the beginning to optimize step set of possible pausing sites
 	no_alt = 0
 	while no_alt < n:
 		if int(liste[no_alt][1]) == 1:
@@ -105,11 +130,7 @@ for line in meta:
 		print("There are no alternative structures possible")
 		continue
 
-	step = 0
-	#highscore = 0
-	best_pause = 0
-	possible_length = [5,10,20,50,100]
-	step_set = list(range(no_alt, n-5))
+	# initialize variables for alternative structure search
 	closest_dist = n # find closest structure if pause is not available
 	closest_call = 0
 	closest_pause = 0
@@ -120,14 +141,26 @@ for line in meta:
 	# lists to store the positions for pauses and the length
 	pause_list = []
 	length_list = []
-	# adaptive walk for one seq struc pair
-	# stop after 2n tries for one pausing side -> exp to visit all sides twice
-	while step < len(step_set) and len(pause_list) < 3:
-	# alternative:
-	#while step < n:
 
+	# initialize varibale for adaptive walk/ exhaustive search
+	step = 0
+	best_pause = 0
+	possible_length = [5,10,20,50,100]
+	step_set = list(range(no_alt, n-5)) # exclude 5' and 3' end and sites with no structure alternatives
+
+	if mode == "adaptive":
+		break_cond_1 = 2*len(step_set)
+	else:
+		break_cond_1 = len(step_set)
+
+	# start of adaptive walk/ exhaustive search
+	while step < break_cond_1 and len(pause_list) < 3:
+	
 		# select pausing side for step
-		new_pause = step_set[step]
+		if mode == "adaptive":
+			new_pause = random.choice(step_set)
+		else:
+			new_pause = step_set[step]
 
 		# check if equilibrium is reached
 		id = new_pause - 1
@@ -150,7 +183,7 @@ for line in meta:
 			step += 1
 			continue
 
-		
+		# test maximum of five lengths at current site
 		for i in possible_length:
 			pause_len = i
 
@@ -161,7 +194,7 @@ for line in meta:
 					pause_call = pause_call + " "+str(pause_list[l])+"="+str(length_list[l])
 			pause_call = pause_call + " "+str(new_pause)+"="+str(pause_len)
 			# call DrTransformer with pausing site
-			subprocess.call(["echo "+seq+" | DrTransformer --logfile --pause-sites "+pause_call], shell=True)
+			subprocess.call(["echo "+seq+" | DrTransformer --logfile --pause-sites "+pause_call + " --o-prune " + str(prune) + " -T " + str(temp)], shell=True)
 
 			# read in list from DrTransformer logfile
 			liste, liste_end = dr_list("NoName.log")
@@ -171,12 +204,12 @@ for line in meta:
 			# find structure rate at end of transcritption
 			for i in range(0,len(liste_end)):
 				if liste_end[i][2] == struc:
-					new_ratio = float(liste_end[i][6])
+					new_ratio = float(liste_end[i][4])
 					break 	
 				elif RNA.bp_distance(struc, liste_end[i][2]) < closest_dist:
 					closest_struc = liste_end[i][2]
 					closest_dist = RNA.bp_distance(struc, liste_end[i][2])
-					closest_ratio = float(liste_end[i][6])
+					closest_ratio = float(liste_end[i][4])
 					closest_pause = new_pause
 					closest_length = pause_len
 
@@ -185,8 +218,9 @@ for line in meta:
 				pause_list.append(new_pause)
 				length_list.append(pause_len)
 				highscore = new_ratio
-				step_set.remove(new_pause)
-				step = 0
+				if mode == "adaptive":
+					step_set.remove(new_pause)
+					step = 0
 				break
 
 
@@ -224,16 +258,9 @@ for line in meta:
 		closest_call =str(closest_dist)+","+closest_struc+","+str(closest_pause)+"="+str(closest_length)+","+str(closest_ratio)
 
 	# save results
-	#with open('result.txt', 'a+') as f:
-	#	f.writelines(seq + " "+ struc+ " "+str(highscore)+ " "+ pause_call + "\n")
 	with open('result.csv', 'a+') as f:
 		# sequence/structure/old occupancy/new occupancy/pauses/closest distance/closest structure/closest pause/closest occupancy
 		f.writelines(seq + ","+ struc+ ","+str(old_highscore)+","+str(highscore)+ ","+ pause_call +"," + closest_call + "\n")
 	print(str(highscore)+ ","+ pause_call + "\n")
 
 	print("--- %s seconds ---" % (time.time() - start_time))
-
-	# save steps for later processing
-	#with open('log/'+struc+'pause.txt', 'a+') as f:
-	#	for i in range(0, len(pause_list)):
-	#		f.writelines(str(pause_list[i]) + " " + str(pause_res[i])+"\n")
--- code/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-seq = sys.argv[1]
-struc = sys.argv[2]
-# get argument from command line
-print(seq)
-print(struc)
